54426ef890c3479420388d1c68f6492d9957d8f3
Daniel Andújar <daniandujar@gmail.com>
2019-11-09 17:24:35 +0100

    ejercicio 4n

--- frontend/src/services/room.service.ts
+++ frontend/src/services/room.service.ts
@@ -1,24 +1,25 @@
 import {AxiosResponse} from 'axios';
 import {Room} from '@/classes/room';
+import {HttpClient} from '@/services/http-client.service';
 
 class RoomsService {
 
   private readonly baseEndPoint: string = '/rooms';
 
-  // @ts-ignore
   public getRooms(): Promise<AxiosResponse<Room[]>> {
+    return HttpClient.get<Room[]>(this.baseEndPoint);
   }
 
-  // @ts-ignore
   public createRoom(room: Room): Promise<AxiosResponse<Room>> {
+    return HttpClient.post<Room>(this.baseEndPoint, room);
   }
 
-  // @ts-ignore
   public updateRoom(room: Room): Promise<AxiosResponse<Room>> {
+    return HttpClient.put<Room>(this.baseEndPoint, room);
   }
 
-  // @ts-ignore
   public deleteRoom(room: Room): Promise<AxiosResponse<Room>> {
+    return HttpClient.delete<Room>(this.baseEndPoint + `/${room.id}`);
   }
 
 }
